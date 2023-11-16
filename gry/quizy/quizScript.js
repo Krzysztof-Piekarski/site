5e0cb764b3945e8f62bbe3f09d1543a5af540841
Krzysztof Piekarski <97312068+Krzysztof-Piekarski@users.noreply.github.com>
2023-11-16 21:34:44 +0100

    answer presenting

--- gry/quizy/quizScript.js
+++ gry/quizy/quizScript.js
@@ -4,6 +4,7 @@ const exitPopupInfo = document.querySelector('.exit-popup-info');
 const main = document.querySelector('.main');
 const continuePopupInfo = document.querySelector('.continue-popup-info');
 const quizSection = document.querySelector('.quiz-section');
+const quizBox = document.querySelector('.quiz-box');
 
 quizStartBtn.onclick = () => {
     quizPopupInfo.classList.add('active');
@@ -16,7 +17,61 @@ exitPopupInfo.onclick = () => {
 }
 
 continuePopupInfo.onclick = () => {
-    quizSection.classList.add('active');
     quizPopupInfo.classList.remove('active');
     main.classList.remove('active');
+    quizBox.classList.add('active');
+    quizSection.classList.add('active');
+
+    showQuestions(0);
+}
+
+let questionCount = 0;
+let wynik = 0;
+
+const nextBtn = document.querySelector('.quiz-next');
+
+nextBtn.onclick = () => {
+    if (questionCount < pytania.length - 1) {
+        questionCount++;
+        showQuestions(questionCount);
+    }
+    else {
+        console.log('To było ostatnie pytanie.');
+    }
+}
+
+const listaOdpowiedzi = document.querySelector('.odpowiedzi'); 
+
+function showQuestions(index) {
+    const questionText = document.querySelector('.pytanie');
+    questionText.textContent = `${pytania[index].numb}. ${pytania[index].pytanie}`;
+
+    let odpowiedz = `<div class="odpowiedz"><span>${pytania[index].opcje[0]}</span></div>
+                    <div class="odpowiedz"><span>${pytania[index].opcje[1]}</span></div>
+                    <div class="odpowiedz"><span>${pytania[index].opcje[2]}</span></div>
+                    <div class="odpowiedz"><span>${pytania[index].opcje[3]}</span></div>`;
+
+    listaOdpowiedzi.innerHTML = odpowiedz;
+
+    const odpowiedzWybrana = document.querySelectorAll('.odpowiedz');
+    for (let i = 0; i < 4; ++i) {
+        odpowiedzWybrana[i].setAttribute('onclick', 'selectOption(this)');
+    }
+
+    const numerPytania = document.querySelector('.question-total');
+    numerPytania.textContent = `${pytania[index].numb} z ${pytania.length} pytań`;
+}
+
+function selectOption(odp) {
+    let odpowiedzKliknieta = odp.textContent;
+    if (odpowiedzKliknieta == pytania[questionCount].odpowiedzPoprawna) {
+        odp.classList.add('poprawna');
+        wynik++;
+    }
+    else {
+        odp.classList.add('bledna');
+    }
+
+    const actualScore = document.querySelector('.header-score');
+    actualScore.textContent = `Wynik: ${wynik}/${pytania.length}`
 }
